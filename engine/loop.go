9fa266dbf8a049a421f48df64e82728c73ae51a8
Matthew <ikirik59@gmail.com>
2022-06-10 19:06:25 +0300

    Implement basic loop listening

--- engine/loop.go
+++ engine/loop.go
@@ -3,14 +3,24 @@ package engine
 import "fmt"
 
 type EventLoop struct {
+	commands *commandsQueue
 }
 
 func (loop *EventLoop) Start() {
 	fmt.Print("I am loop start")
+	loop.commands = new(commandsQueue)
+	go loop.listen()
 }
 
-func (loop *EventLoop) Push() {
-	fmt.Print("I am loop push")
+func (loop *EventLoop) listen() {
+	for !loop.commands.isEmpty() {
+		cmd := loop.commands.pull()
+		cmd.Execute(loop)
+	}
+}
+
+func (loop *EventLoop) Post(cmd Command) {
+	fmt.Print("I am loop post")
 }
 
 func (loop *EventLoop) AwaitFinish() {
