a621d97f1d036747374c0ea7224e52d37207ae6d
Matthew <ikirik59@gmail.com>
2022-06-11 14:29:56 +0300

    Add basic stress test for EventLoop

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/MatthewKirik/architecture-lab-4/engine"
+)
 
 func main() {
-	fmt.Print("Hello world!")
+	eventLoop := new(engine.EventLoop)
+	eventLoop.Start()
+	eventLoop.Post(&printCommand{"Hello"})
+	eventLoop.Post(&printCommand{"World"})
+	eventLoop.Post(&printCommand{"!"})
+	eventLoop.Post(&splitCmd{"split me, please", ","})
+	for i := 0; i < 1000000; i++ {
+		csvText := fmt.Sprintf("i am,a csv,file,29-11-29292,%v", i)
+		eventLoop.Post(&splitCmd{csvText, ","})
+	}
+	eventLoop.AwaitFinish()
 }
