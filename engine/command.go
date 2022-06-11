40b9304e3e658c457cad80c47e5ad1fe1ff8c621
Matthew <ikirik59@gmail.com>
2022-06-11 13:28:33 +0300

    Add generic function command

--- engine/command.go
+++ engine/command.go
@@ -3,3 +3,9 @@ package engine
 type Command interface {
 	Execute(handler Handler)
 }
+
+type FuncCommand func(handler Handler)
+
+func (fCmd FuncCommand) Execute(handler Handler) {
+	fCmd(handler)
+}
