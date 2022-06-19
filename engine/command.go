0ef284de68725036518b0a69138b2652b54242d9
Matthew <ikirik59@gmail.com>
2022-06-19 19:17:55 +0300

    Fixed not waiting for callbacks after AwaitFinish

--- engine/command.go
+++ engine/command.go
@@ -4,8 +4,8 @@ type Command interface {
 	Execute(handler Handler)
 }
 
-type FuncCommand func(handler Handler)
+type FuncCommand func()
 
 func (fCmd FuncCommand) Execute(handler Handler) {
-	fCmd(handler)
+	fCmd()
 }
--- engine/loop.go
+++ engine/loop.go
@@ -5,35 +5,66 @@ import "sync"
 type EventLoop struct {
 	commands      *commandsQueue
 	stopRequested bool
-	stopped       chan struct{}
+	stopped       bool
+	stopLocker    sync.Mutex
+	stopCond      sync.Cond
 }
 
 func (loop *EventLoop) Start() {
 	loop.commands = &commandsQueue{
 		cond: *sync.NewCond(&sync.Mutex{}),
 	}
-	loop.stopped = make(chan struct{})
+	loop.stopCond = *sync.NewCond(&sync.Mutex{})
 	go loop.listen()
 }
 
 func (loop *EventLoop) listen() {
-	for !loop.stopRequested || !loop.commands.isEmpty() {
+	for !loop.isStopRequested() || !loop.commands.isEmpty() {
 		cmd := loop.commands.pull()
-		cmd.Execute(loop)
+		cmd.Execute(&trustedHandler{loop})
 	}
-	loop.stopped <- struct{}{}
+	loop.stopLocker.Lock()
+	loop.stopped = true
+	loop.stopLocker.Unlock()
+	loop.stopCond.Broadcast()
+}
+
+func (loop *EventLoop) isStopRequested() bool {
+	loop.stopLocker.Lock()
+	defer loop.stopLocker.Unlock()
+	return loop.stopRequested
+}
+
+func (loop *EventLoop) isStopped() bool {
+	loop.stopLocker.Lock()
+	defer loop.stopLocker.Unlock()
+	return loop.stopped
 }
 
 func (loop *EventLoop) Post(cmd Command) {
-	if loop.stopRequested {
+	if loop.isStopRequested() {
 		return
 	}
 	loop.commands.push(cmd)
 }
 
 func (loop *EventLoop) AwaitFinish() {
-	loop.Post(FuncCommand(func(handler Handler) {
+	if loop.isStopped() {
+		return
+	}
+	if !loop.isStopRequested() {
+		loop.stopLocker.Lock()
 		loop.stopRequested = true
-	}))
-	<-loop.stopped
+		loop.stopLocker.Unlock()
+	}
+	loop.stopCond.L.Lock()
+	loop.stopCond.Wait()
+}
+
+type trustedHandler struct {
+	loop *EventLoop
+}
+
+func (th *trustedHandler) Post(cmd Command) {
+	th.loop.commands.push(cmd)
 }
