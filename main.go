364ed539c80a7bab1d657da85635bbd9f260be93
Matthew <ikirik59@gmail.com>
2022-06-21 22:17:28 +0300

    Fix style

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"fmt"
 
+	"github.com/MatthewKirik/architecture-lab-4/command"
 	"github.com/MatthewKirik/architecture-lab-4/engine"
 )
 
 func main() {
 	eventLoop := new(engine.EventLoop)
 	eventLoop.Start()
-	eventLoop.Post(&printCommand{"Hello"})
-	eventLoop.Post(&printCommand{"World"})
-	eventLoop.Post(&printCommand{"!"})
-	eventLoop.Post(&splitCmd{"split me, please", ","})
+	eventLoop.Post(&command.PrintCmd{Text: "Hello"})
+	eventLoop.Post(&command.PrintCmd{Text: "World"})
+	eventLoop.Post(&command.PrintCmd{Text: "!"})
+	eventLoop.Post(&command.SplitCmd{Text: "split me, please", Separator: ","})
 	for i := 0; i < 1000000; i++ {
 		csvText := fmt.Sprintf("i am,a csv,file,29-11-29292,%v", i)
-		eventLoop.Post(&splitCmd{csvText, ","})
+		eventLoop.Post(&command.SplitCmd{Text: csvText, Separator: ","})
+		if i == 20000 {
+			eventLoop.AwaitFinish()
+		}
 	}
-	eventLoop.AwaitFinish()
+	// eventLoop.AwaitFinish()
 }
