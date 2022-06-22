d97475db674a9a212418ee9c04a2821f86e4cfa7
gurug-prog <ivanmysterys@gmail.com>
2022-06-22 15:52:55 +0300

    feat: implement event loop handling

--- main.go
+++ main.go
@@ -1,25 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"flag"
+	"log"
+	"os"
 
 	"github.com/MatthewKirik/architecture-lab-4/command"
 	"github.com/MatthewKirik/architecture-lab-4/engine"
 )
 
+var inputFile = flag.String("f", "./assets/commands-examples.txt",
+	"Input file with loop commands")
+
 func main() {
+	flag.Parse()
+
+	input, err := os.Open(*inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer input.Close()
+
 	eventLoop := new(engine.EventLoop)
 	eventLoop.Start()
-	eventLoop.Post(&command.PrintCmd{Text: "Hello"})
-	eventLoop.Post(&command.PrintCmd{Text: "World"})
-	eventLoop.Post(&command.PrintCmd{Text: "!"})
-	eventLoop.Post(&command.SplitCmd{Text: "split me, please", Separator: ","})
-	for i := 0; i < 1000000; i++ {
-		csvText := fmt.Sprintf("i am,a csv,file,29-11-29292,%v", i)
-		eventLoop.Post(&command.SplitCmd{Text: csvText, Separator: ","})
-		if i == 20000 {
-			eventLoop.AwaitFinish()
-		}
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		commandLine := scanner.Text()
+		cmd := command.Parse(commandLine)
+		eventLoop.Post(cmd)
 	}
-	// eventLoop.AwaitFinish()
+	eventLoop.AwaitFinish()
 }
