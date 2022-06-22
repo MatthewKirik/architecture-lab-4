33bb6217ae2ea4079f555c0d704297d65814c65a
gurug-prog <ivanmysterys@gmail.com>
2022-06-22 15:04:18 +0300

    feat: add basic command tests

--- command/parser_test.go
+++ command/parser_test.go
@@ -6,16 +6,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var testsPath = "../tests/parset-tests.txt"
+// var testsPath = "../tests/parser-tests.txt"
 
 func TestParsePrintCmd(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal("", "")
+	inputStr := "print hello!"
+	expected := "hello!"
+
+	cmd := Parse(inputStr)
+
+	assert.Equal(cmd.(*PrintCmd).Text, expected)
 }
 
 func TestParseSplitCmd(t *testing.T) {
 	assert := assert.New(t)
-	assert.Equal("", "")
+	inputStr := "split hey!dude!split!me !"
+	expectedStr := "hey!dude!split!me"
+	expectedSep := "!"
+
+	cmd := Parse(inputStr)
+
+	assert.Equal(cmd.(*SplitCmd).Text, expectedStr)
+	assert.Equal(cmd.(*SplitCmd).Separator, expectedSep)
 }
 
 func TestErrorSplitCmdLongSeparator(t *testing.T) {
